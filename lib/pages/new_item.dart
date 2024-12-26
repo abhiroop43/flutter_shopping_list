3901e0a9876e3be1b2a81d4254fac6e2a812cd12
Abhiroop Santra <abhiroop.santra@gmail.com>
2024-12-26 15:51:51 +0400

    feat: enhance new item page with form fields for name, quantity, and category selection

--- lib/pages/new_item.dart
+++ lib/pages/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItemPage extends StatefulWidget {
   const NewItemPage({super.key});
@@ -16,9 +17,86 @@ class _NewItemPageState extends State<NewItemPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: const Center(
-          child: Text('Add New Item'),
-        ),
+        child: Form(
+            child: Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              decoration: const InputDecoration(labelText: 'Name'),
+              validator: (value) {
+                return value!.isEmpty ? 'Please enter a name' : null;
+              },
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    maxLength: 3,
+                    decoration: const InputDecoration(labelText: 'Quantity'),
+                    keyboardType: TextInputType.number,
+                    initialValue: '1',
+                    validator: (value) {
+                      return value!.isEmpty ? 'Please enter a quantity' : null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (var category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.categoryColor,
+                                ),
+                                SizedBox(width: 8),
+                                Text(category.value.name),
+                              ],
+                            ),
+                          )
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.tertiary,
+                    foregroundColor: Theme.of(context).colorScheme.onTertiary,
+                  ),
+                  child: const Text('Reset'),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                  onPressed: () {},
+                  child: const Text('Save'),
+                ),
+              ],
+            )
+          ],
+        )),
       ),
     );
   }
