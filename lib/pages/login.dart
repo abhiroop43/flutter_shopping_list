4830e874381fb7bed1399ff72aa6aae864ab895b
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-01-25 10:09:20 +0400

    feat: completed login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shopping_list/pages/home.dart';
+import 'package:shopping_list/providers/login_provider.dart';
 
 class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
@@ -9,15 +11,95 @@ class LoginPage extends ConsumerStatefulWidget {
 }
 
 class _LoginPageState extends ConsumerState<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool formBeingSubmitted = false;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    //void errorSnackBar = showErrorSnackBar('Error logging in.');
+
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    setState(() {
+      formBeingSubmitted = true;
+    });
+
+    debugPrint(
+        'Submitting form with email: ${emailController.text} and password: ${passwordController.text}');
+
+    ref
+        .read(loginProvider.notifier)
+        .loginUser(emailController.text, passwordController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var isLoggedIn = ref.watch(loginProvider);
+
+    if (isLoggedIn.isLoggedIn) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => HomePage()));
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Login',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: const Text(
+            'Login',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(labelText: 'Email'),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an email address';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                    child: const Text('Login'),
+                  )
+                ],
+              )),
+        ));
   }
 }
